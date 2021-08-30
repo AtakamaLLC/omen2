10c3ee0e6721d5388692d38355ca75cd95f08c3a
erik aronesty <erik@q32.com>
2021-08-30 11:07:18 -0400

    select by attr works

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def long_description():
 
 setup(
     name="omen2",
-    version="1.0.5",
+    version="1.0.6",
     description="Database object & cache manager",
     packages=["omen2"],
     long_description=long_description(),
