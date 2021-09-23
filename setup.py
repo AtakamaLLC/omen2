f823d099af04e8c24611c7555472e24b13e5fb1f
erik aronesty <erik@q32.com>
2021-09-23 11:16:00 -0400

    bump ver not fwd compat

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def long_description():
 
 setup(
     name="omen2",
-    version="1.1.3",
+    version="1.2.1",
     description="Database object & cache manager",
     packages=["omen2"],
     long_description=long_description(),
