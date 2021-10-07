68868a33f1f807a98ce0228024fdbe8cf1dc63c4
erik aronesty <erik@q32.com>
2021-10-07 12:51:26 -0400

    comment

--- omen2/errors.py
+++ omen2/errors.py
@@ -31,4 +31,5 @@ class OmenLockingError(Exception):
     """Deadlock detection.  If this is thrown, the system should drop out/die/fail hard."""
 
 
+# has to be the same, or we have to track down every place where they *might not be*
 IntegrityError = notanorm.errors.IntegrityError
